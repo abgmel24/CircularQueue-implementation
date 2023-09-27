8a869f5c7aff3a68b8eb12e241b9506364c7fcb6
abgmel24 <abhigudimella@gmail.com>
2023-09-26 17:43:42 -0700

    Finished and tested

---
+++ genCircularQueue.cpp
@@ -0,0 +1,61 @@
+#include "genqueue.h"
+
+GenQueue::GenQueue() {
+    size = 64;
+    array = new int[size];
+    front = -1;
+    rear = 0;
+}
+
+GenQueue::GenQueue(int maxSize) {
+    size = maxSize;
+    array = new int[size];
+    front = 0;
+    rear = 0;
+}
+
+GenQueue::~GenQueue() {
+    delete[] array;
+    cout << "garbage collecting"
+}
+
+void GenQueue::insert(char c) {
+    if(isFull()) {
+        throw runtime_error("Circular Queue is full");
+    }
+
+    array[rear++] = c;
+    rear %= size;
+    ++numElements;
+}
+
+char GenQueue::remove() {
+    if(isEmpty()) {
+        //TODO
+    }
+    //TODO
+    numElements--;
+    return array[front++];
+}
+
+char GenQueue::front() {
+    return array[front];
+}
+
+unsigned int GenQueue::getSize() {
+    return numElements;
+}
+
+bool GenQueue::isEmpty() {
+    return front == 0;
+}
+
+bool GenQueue::isFull() {
+    return numElements == size;
+}
+
+void GenQueue::printQueue() {
+    for(int i = front; i < rear; ++i) {
+        cout << array[i] << endl;
+    }
+}
